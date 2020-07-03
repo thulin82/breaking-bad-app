d20ae7a1bb30a4205a4a619194fd4ca04945f1da
Markus Thulin <macky_b@hotmail.com>
2020-07-03 11:10:57 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './components/ui/Header';
+import './App.css';
+
+const App = () => {
+    return (
+        <div className="container">
+            <Header />
+        </div>
+    );
+};
+
+export default App;
